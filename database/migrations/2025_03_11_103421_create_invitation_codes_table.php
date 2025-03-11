6635f66c44a36678d5cef6906807e5d0fb13a729
AmirHosseinAghaei82 <hosseinaqaei931@gmail.com>
2025-03-11 14:28:04 +0330

    Migrate Invite Code

---
+++ database/migrations/2025_03_11_103421_create_invitation_codes_table.php
@@ -0,0 +1,31 @@
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+
+        Schema::create('invitation_codes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sender_id');
+            $table->unsignedBigInteger('receiver_id');
+            $table->integer('invitation_code');
+            $table->boolean('code_used')->default(0);
+            $table->timestamps();
+        });
+
+    }
+
+    public function down(): void
+    {
+
+        Schema::dropIfExists('invitation_codes');
+
+    }
+};
